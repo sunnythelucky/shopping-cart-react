d5ad714c47b4b69c40f11088b57656f865044035
sunnythelucky <sunnyparkk@gmail.com>
2023-06-27 22:55:46 -0400

    Add basename

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
+import { useRouterHistory } from "react-router";
+import { createHistory } from "history";
 
 function App() {
-  return (
-    <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Router>
-      </ShopContextProvider>
-    </div>
-  );
+	const history = useRouterHistory(createHistory)({
+		basename: "/shopping-cart-react",
+	});
+
+	return (
+		<div className="App">
+			<ShopContextProvider>
+				<Router history={history}>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Shop />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/cart" element={<Cart />} />
+					</Routes>
+				</Router>
+			</ShopContextProvider>
+		</div>
+	);
 }
 
 export default App;
