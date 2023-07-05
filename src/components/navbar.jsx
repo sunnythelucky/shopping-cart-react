0f5308f7558c133d7ad42ae4c981e10d826c62bb
sunnythelucky <sunnyparkk@gmail.com>
2023-07-04 22:53:00 -0400

    Add the total Amount to cart icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../context/shop-context";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
 
 export const Navbar = () => {
+	const { getTotalCartAmount } = useContext(ShopContext);
+	const totalAmount = getTotalCartAmount();
 	return (
 		<div className="navbar">
 			<div className="links">
@@ -11,6 +14,7 @@ export const Navbar = () => {
 				<Link to="/shopping-cart-react/contact"> Contact </Link>
 				<Link to="/shopping-cart-react/cart">
 					<ShoppingCart size={32} />
+					<div>{totalAmount > 0 && totalAmount}</div>
 				</Link>
 			</div>
 		</div>
