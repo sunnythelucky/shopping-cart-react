cdbaf75b120db61183b49c23fc666a6ac00336c8
sunny park <sunnyparkk@gmail.com>
2023-07-25 15:45:04 -0400

    Add Contact and Login to cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,41 +4,56 @@ import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
 
 import "./cart.css";
+import { Contact } from "../contact/contact";
+import { Login } from "../login/login";
 export const Cart = () => {
-	const { cartItems, getTotalCartAmount, checkout, products } = useContext(ShopContext);
+	const {
+		cartItems,
+		getTotalCartAmount,
+		checkout,
+		products,
+		isContactOpen,
+		setIsContactOpen,
+		isLoginOpen,
+		setIsLoginOpen,
+	} = useContext(ShopContext);
 	const totalAmount = getTotalCartAmount();
 	const navigate = useNavigate();
 
 	return (
-		<div className="cart">
-			<div>
-				<h1>Your Cart Items</h1>
-			</div>
+		<>
+			<Contact open={isContactOpen} onClose={() => setIsContactOpen(false)}></Contact>
+			<Login open={isLoginOpen} onClose={() => setIsLoginOpen(false)}></Login>
 			<div className="cart">
-				{products.map((product) => {
-					if (cartItems[product.id] > 0) {
-						return <CartItem data={product} key={product.key} />;
-					}
-					return null;
-				})}
-			</div>
-
-			{totalAmount > 0 ? (
-				<div className="checkout">
-					<p> Subtotal: ${totalAmount} </p>
-					<button onClick={() => navigate("/")}> Continue Shopping </button>
-					<button
-						onClick={() => {
-							checkout();
-							navigate("/checkout");
-						}}
-					>
-						Checkout
-					</button>
+				<div>
+					<h1>Your Cart Items</h1>
 				</div>
-			) : (
-				<h1> Your Shopping Cart is Empty</h1>
-			)}
-		</div>
+				<div className="cart">
+					{products.map((product) => {
+						if (cartItems[product.id] > 0) {
+							return <CartItem data={product} key={product.key} />;
+						}
+						return null;
+					})}
+				</div>
+
+				{totalAmount > 0 ? (
+					<div className="checkout">
+						<p> Subtotal: ${totalAmount} </p>
+						<button onClick={() => navigate("/")}> Continue Shopping </button>
+						<button
+							onClick={() => {
+								checkout();
+								navigate("/checkout");
+							}}
+						>
+							Checkout
+						</button>
+					</div>
+				) : (
+					<h1> Your Shopping Cart is Empty</h1>
+				)}
+			</div>
+		</>
 	);
 };
